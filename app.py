034df00709eff962e64133ffde8ce3b1ba9e6c29
25f2009371-oss <25f2009371@ds.study.iitm.ac.in>
2026-08-07 21:08:18 +0530

    Creating database succefully with required tables

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
-from flask import Flask, render_template
-app=Flask(__name__)
+from flask import Flask
+from models import db
 
+app = None
 
+def setup_app():
+    global app
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///databse.sqlite3"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
+    app.app_context().push()
+    print("Database setup done ... ")
 
+setup_app()
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(debug=True)
--- models.py
+++ models.py
@@ -2,8 +2,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-
 class User(db.Model):
+    __tablename__ = "users"
     user_id = db.Column(db.Integer, primary_key=True)
     full_name = db.Column(db.String, nullable=False)
     email = db.Column(db.String, unique=True, nullable=False)
@@ -11,26 +11,28 @@ class User(db.Model):
     phone = db.Column(db.String, nullable=False)
     role = db.Column(db.String, nullable=False)
     status = db.Column(db.String, nullable=False)
-
+    bookings = db.relationship("Booking", cascade="all,delete", backref="user")
 
 class Trek(db.Model):
+    __tablename__ = "treks"
     trek_id = db.Column(db.Integer, primary_key=True)
     trek_name = db.Column(db.String, nullable=False)
     location = db.Column(db.String, nullable=False)
     difficulty = db.Column(db.String, nullable=False)
-    duration = db.Column(db.Integer, nullable=False)
+    duration = db.Column(db.String, nullable=False)
     start_date = db.Column(db.Date, nullable=False)
     end_date = db.Column(db.Date, nullable=False)
     total_slots = db.Column(db.Integer, nullable=False)
     available_slots = db.Column(db.Integer, nullable=False)
     status = db.Column(db.String, nullable=False)
-    description = db.Column(db.String)
-    assigned_staff_id = db.Column(db.Integer, nullable=False)
-
+    description = db.Column(db.String, nullable=False)
+    assigned_staff_id = db.Column(db.Integer, nullable=True)
+    bookings = db.relationship("Booking", cascade="all,delete", backref="trek")
 
 class Booking(db.Model):
+    __tablename__ = "bookings"
     booking_id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, nullable=False)
-    trek_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"), nullable=False)
+    trek_id = db.Column(db.Integer, db.ForeignKey("treks.trek_id"), nullable=False)
     booking_date = db.Column(db.Date, nullable=False)
     status = db.Column(db.String, nullable=False)
